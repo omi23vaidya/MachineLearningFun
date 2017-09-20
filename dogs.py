bb364bc914f1d3cfbf184ca2f89401434edf6846
Omkar Vaidya <omi23.vaidya@gmail.com>
2017-09-20 15:20:23 -0400

    learning how the selection of feature matters

---
+++ dogs.py
@@ -0,0 +1,18 @@
+##Just trying to see what makes a good feature
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+greyhounds = 500
+labs = 500
+
+#greyhound's height would be between 24 and 32
+greyhound_height = 28 + 4 * np.random.randn(greyhounds)
+
+#lab's height would be between 20 and 28
+lab_height = 24 + 4 * np.random.randn(labs)
+# print(greyhound_height)
+# print(lab_height)
+
+# red color for greyhounds and blue for labs 
+plt.hist([greyhound_height, lab_height], stacked = True, color = ['r','b'])
+plt.show() 
